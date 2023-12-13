ce221c2bf497b80bdb3f3fc9f4600744a43a4606
RobertBobert562 <robert2000torresg@gmail.com>
2023-12-12 17:48:30 -0800

    0.0.3

--- Robert_Torres/contacts/urls.py
+++ Robert_Torres/contacts/urls.py
@@ -2,9 +2,9 @@
 # contacts/urls.py
 
 from django.urls import path
-
 from .views import ContactListView
 
 urlpatterns = [
+    path("create/", ContactCreateView.as_view(), name="contact_create"),
     path("", ContactListView.as_view(), name="contact_list"),
 ]
--- Robert_Torres/contacts/views.py
+++ Robert_Torres/contacts/views.py
@@ -6,11 +6,15 @@ from django.shortcuts import render
 def start_page(request):
     return render(request, 'contacts/start_page.html', {'contacts': []})
 
-
-from django.views.generic import ListView
-
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from .models import Contact
+from .forms import ContactForm
 
+class ContactCreateView(CreateView):
+    model = Contact
+    form_class = ContactForm
+    success_url = reverse_lazy("contact_list")
 
 class ContactListView(ListView):
     model = Contact
